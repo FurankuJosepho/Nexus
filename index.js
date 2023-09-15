c804d24bee594d2e489782e803cc4512448294e2
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-15 19:39:37 +0800

    Additions

---
+++ index.js
@@ -0,0 +1,30 @@
+// navbar
+const navbarMenu = document.querySelector(".navbar .links");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = formPopup.querySelector(".close-btn");
+// Show mobile menu
+hamburgerBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+
+// Hide mobile menu
+hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+// toggle
+const body = document.querySelector("body"),
+        toggle = document.querySelector(".toggle");
+      let getMode = localStorage.getItem("mode");
+      if (getMode && getMode === "dark") {
+        body.classList.add("dark");
+        toggle.classList.add("active");
+      }
+      toggle.addEventListener("click", () => {
+        body.classList.toggle("dark");
+        if (!body.classList.contains("dark")) {
+          return localStorage.setItem("mode", "light");
+        }
+        localStorage.setItem("mode", "dark");
+      });
+      toggle.addEventListener("click", () => toggle.classList.toggle("active"));
