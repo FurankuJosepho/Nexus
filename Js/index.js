81b3ea88c503f37dc9877945e62b64a6dc21c7f6
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-22 22:25:22 +0800

    Added swiper js

--- Js/index.js
+++ Js/index.js
@@ -12,3 +12,18 @@ hamburgerBtn.addEventListener("click", () => {
 
 // Hide mobile menu
 hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+
+// Swiper JS
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
