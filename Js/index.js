5a11b3af1f77188eb3b9e6cc2771ee12419fc45b
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-23 23:30:05 +0800

    Added another swiper js

--- Js/index.js
+++ Js/index.js
@@ -14,7 +14,7 @@ hamburgerBtn.addEventListener("click", () => {
 hideMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
 // Swiper JS
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   effect: "fade",
@@ -25,5 +25,33 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+});
+
+// Swiper JS 2
+let swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 100,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
   },
 });
