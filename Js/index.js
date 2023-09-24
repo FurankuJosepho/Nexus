560bcef924e6190264f6c24f568588db6aa080de
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-24 23:52:39 +0800

    Added somethings finish product if tama na soon to add

--- Js/index.js
+++ Js/index.js
@@ -14,31 +14,28 @@ hamburgerBtn.addEventListener("click", () => {
 hideMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
 // Swiper JS
+// Swiper JS 1
 let swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   effect: "fade",
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next", // Unique class for swiper 1 next button
+    prevEl: ".swiper-button-prev", // Unique class for swiper 1 prev button
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination-1", // Unique class for swiper 1 pagination
     clickable: true,
   },
 });
 
 // Swiper JS 2
 let swiper2 = new Swiper(".mySwiper2", {
-  slidesPerView: 1,
-  spaceBetween: 100,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  slidesPerView: 5,
+  spaceBetween: 50,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next-2", // Unique class for swiper 2 next button
+    prevEl: ".swiper-button-prev-2", // Unique class for swiper 2 prev button
   },
   breakpoints: {
     640: {
@@ -46,11 +43,11 @@ let swiper2 = new Swiper(".mySwiper2", {
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 40,
     },
     1024: {
-      slidesPerView: 5,
+      slidesPerView: 3.5,
       spaceBetween: 50,
     },
   },
